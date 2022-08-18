55e7854830b6780df66f21517bd930e93b31c3ce
Rian Sanjaya <rian.sj@gmail.com>
2022-08-18 16:27:50 +0700

    adding progress indicator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
 import { AppProps } from "next/app";
 // import type { ReactElement, ReactNode } from 'react'
 // import type { NextPage } from 'next'
 
+import 'nprogress/nprogress.css'; //styles of nprogress
 import "@styles/global.css";
 import "@styles/vars.css";
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     const MComponent: any = Component;
